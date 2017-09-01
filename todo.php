12d720074a022ed09b3198fdcd9f79c9f00ac141
jeffreytabor <jeffreytabor@gmail.com>
2017-09-01 12:08:46 -0400

    Update todo.php

--- todo.php
+++ todo.php
@@ -4,4 +4,8 @@ Each div needs a clear name, nav, body, and debug footer
 
 Make readme with dillinger and all code gets block comments
 
+Create function to handle data, and everything has to be JavaScript, no php. 
+
+Needs to be able to all run in folder on desktop via browser. One input and output folder
+
 
